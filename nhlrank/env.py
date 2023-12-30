55c79b805dca51eb1320b62c0eeaac0aa61a5092
Shane Jaroch <chown_tee@proton.me>
2023-12-30 14:22:06 -0500

    add todo in env.py

--- nhlrank/env.py
+++ nhlrank/env.py
@@ -14,3 +14,10 @@ dotenv.load_dotenv(verbose=True)
 CSV_GAMES_URL = (
     "https://shanemcd.org/wp-content/uploads/2023/08/nhl-202324-asplayed.csv"
 )
+
+# TODO: override command line args with ENV vars from .env file
+#       (e.g. `--debug` should be able to be set in .env file)
+#       (e.g. `--num_games` should be able to be set in .env file)
+#       (e.g. `--sort_column` should be able to be set in .env file)
+#       (e.g. `--team` should be able to be set in .env file)
+#       (e.g. `--skip_dl` should be able to be set in .env file)
