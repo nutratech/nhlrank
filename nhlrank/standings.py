da39bff85f5e23083451e272978ecbb7cbab69f7
Shane Jaroch <chown_tee@proton.me>
2024-01-01 13:48:58 -0500

    continue working

--- nhlrank/standings.py
+++ nhlrank/standings.py
@@ -84,69 +84,27 @@ def standings_by_div(
 
     for conf, divs in constants.conference_and_division_organization.items():
         print_title(conf)
+        print(list(divs.values()))
+        teams_conf = [
+            team for team in teams if team in ([].extend(divs.values()))
+        ]
+        for div
+        print(teams_conf)
+        exit()
+        non_wildcard_teams = []
         for div, team_abbrev_div in divs.items():
             print_subtitle(div)
             # Take the top 3 teams from each division
-            div_teams = [team for team in teams if team.abbrev in team_abbrev_div][:3]
-            print([x.abbrev for x in div_teams])
-            # standings(div_teams)
+            teams_div = [team for team in teams if team.abbrev in team_abbrev_div][:3]
+            non_wildcard_teams.extend(teams_div)
+            # print([x.abbrev for x in div_teams])
+            standings(teams_div)
+
+        # Take the top 2 teams from the remaining teams
         print_subtitle("Wildcard")
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Create the table
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    table_series_standings = [
-        (
-            i + 1,
-            team.name,
-            team.games_played,
-            team.wins,
-            team.losses,
-            team.losses_ot,
-            team.points,
-            team.points_percentage,
-            team.rating_str.split()[0],
-            team.avg_opp or str(),
-            team.rating_max or str(),
-            team.rating_avg or str(),
-            team.best_win or str(),
-            team.goals_for,
-            team.goals_against,
-            "-".join(str(x) for x in team.record_home),
-            "-".join(str(x) for x in team.record_away),
-            "-".join(str(x) for x in team.shootout),
-            "-".join(str(x) for x in team.last_10),
-            team.streak,
-        )
-        for i, team in enumerate(teams)
-    ]
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Print the rankings table
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    _table = tabulate(
-        table_series_standings,
-        headers=[
-            "#",
-            "Team",
-            "GP",
-            "W",
-            "L",
-            "OTL",
-            "Pts",
-            "P%",
-            "Rate",
-            "Opp",
-            "Top",
-            "Avg",
-            "Best W",
-            "GF",
-            "GA",
-            "Home",
-            "Away",
-            "S/O",
-            "L10",
-            "Run",
-        ],
-    )
-    print(_table)
+        wildcard_teams = [
+            team
+            for team in teams
+            if team not in non_wildcard_teams
+        ][:2]
+        standings(wildcard_teams)
